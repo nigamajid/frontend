401002f2f9f7d2762b6f4a8a58b2d617a39aecf4
nigamajid <74210017+nigamajid@users.noreply.github.com>
2021-01-04 19:07:01 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function onClickMenu() {
+  document.getElementById("menu") .classlist.toggle("change");
+}
+
+
+
+
+
+/*
+function myFunction() {
+  var x = document.querySelector(".hamburgermenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} */
+
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser*/
+
+/*eslint 'no-console': 0*/
